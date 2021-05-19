8aaafbc1a4edeafe85aeeb0a96c6073438a07cfd
PC Diego <marquezdiegogabriel@outlook.com>
2021-05-18 21:50:20 -0300

    Terminado Asincronismo

--- 7. Asincronismo/src/promise/index.js
+++ 7. Asincronismo/src/promise/index.js
@@ -1,25 +1,63 @@
-/**
- * Aqui la promesa se ejecuta al cargar el archivo
-*/
-const somethingWillHapped = new Promise( (resolve, reject) => {
-    if (true) {
-        console.log('Hey dude!');
-        resolve('Hey!');
-    } else {
-        reject('Whoops!');
-    }
-});
-
-/**
- *Aqui la promesa no se ejecuta hasta que se llame a la funcion
-*/
-const somethingWillHapped = () => {
-    return new Promise( (resolve, reject) => {
+//Crear funcion 
+const somethingWillHappen = () => {
+    //retornar una promesa con dos argumentos (resolve=si se ejecuta, reject=si se rechaza)
+    return new Promise((resolve, reject) => {
+        //si es verdadero, vamos a devolver hey
         if (true) {
-            console.log('Hey dude!');
-            resolve('Hey!');
-        } else {
-            reject('Whoops!');
+            resolve('Hey!')
         }
-    });
+        //si no entonces devolvemos wooops
+        else {
+            reject('Woooops!')
+        }
+    })
 }
+//ejecutamos la funcion
+somethingWillHappen()
+    //si estamos obteniendo un resolve
+    .then(response => console.log(response))
+    //si obtenemos un reject
+    .catch(err => console.error(err))
+
+
+
+
+//Crear segunda funcion
+const somethingWillHappen2 = () => {
+    // retuornamos la promesa
+    return new Promise((resolve, reject) => {
+        // Si es verdadero, devolvemos True en 2 segundos
+        if(true) {
+            setTimeout(() => {
+                resolve('True')
+            }, 2000)
+        } 
+        // Si no entonces devolvemos el error
+        // De esta forma "new Error" podemos debbugear mejor
+        else {
+            const error = new Error('Whoppps!')
+            reject(error)
+        }
+    })
+}
+//ejecutamos la Funcion 2
+somethingWillHappen2()
+    //Si obtenemos un resolve
+    .then(response => console.log(response))
+    //si obtenemos un reject
+    .catch(err => console.error(err))
+
+
+
+
+    
+//ejecutamos todas las promesas
+Promise.all([somethingWillHappen(),somethingWillHappen2()])
+    //Si obtenemos un resolve
+    .then(response => {
+        console.log('Array of results', response);
+    })
+    //Si obtenemos un reject
+    .catch(err => {
+        console.error(err)
+    }) 
